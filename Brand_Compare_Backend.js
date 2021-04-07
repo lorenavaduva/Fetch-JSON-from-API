9f0eefb18f65f51c6d70c4c47c981e965cf22d63
lorenavaduva <lorena.vaduva413@gmail.com>
2021-04-07 23:50:00 +0300

    Finished main functionality

--- Brand_Compare_Backend.js
+++ Brand_Compare_Backend.js
@@ -1,7 +1,78 @@
 //I am using express(web framework for node.js) to host static files.
 const express = require("express");
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+console.log(process.env);
+
 
 const app = express();
-app.listen(3000, () => console.log("listening at 3000")); 
-app.use(express.static("public")); 
-app.use(express.json({ limit: "1mb" }));
+app.listen(3000, () => console.log("listening at 3000"));
+app.use(express.static("public"));
+//app.use(express.json({ limit: "1mb" }));
+
+
+
+
+
+
+
+
+app.get("/brands", async (request, response) => {
+    const fetchResponse = await fetch("https://app.socialinsider.io/api", {
+      // fetch getbrands data from API
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.API_KEY,
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        id: 0,
+        method: "partner_api.get_brands",
+        params: { projectname: "Automotive" },
+      }),
+    });
+    const jsonrpc = await fetchResponse.json();
+    console.log(jsonrpc);
+    response.json(jsonrpc);
+  });
+
+
+  app.get("/data/:start_date/:end_date/:brand_name", async (request, response) => {
+    
+    console.log('parametrii:');
+    const StartDate = Number(request.params.start_date);
+    const EndDate = Number(request.params.end_date);
+    console.log(StartDate);
+    const BrandName = request.params.brand_name;
+
+    const fetchResponse = await fetch("https://app.socialinsider.io/api", {
+      // fetch getbrands data from API
+      
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.API_KEY,
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        id: 0,
+        method: "partner_api.get_brand_data",
+        params: { 'projectname': "Automotive" ,
+        brandname: BrandName,
+        date:{start: StartDate, end: EndDate }
+        }
+      }),
+    });
+    const jsonrpc = await fetchResponse.json();
+    console.log(jsonrpc);
+    response.json(jsonrpc);
+  });
+ 
+  /*app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    response.json(data);
+  });*/
---
+++ public/sketch.js
@@ -0,0 +1,92 @@
+document.getElementById("submit").addEventListener("click", async (event) => {
+  const dateControl = document.getElementById("start");
+
+  //const start_date = dateControl.value.split('-');// year month day
+  //console.log(dateControl.valueAsNumber);
+  //console.log(dateControl.value);
+
+  const dateControl2 = document.getElementById("end");
+
+  //console.log(dateControl2.valueAsNumber);
+
+  console.log("Importing brands...");
+
+  const response = await fetch("/brands");
+  const brand_json = await response.json();
+  console.log(brand_json);
+  console.log("Successfully imported brands.");
+
+  console.log("Importing brand data...");
+
+  let start_date = dateControl.valueAsNumber;
+  console.log(start_date);
+
+  let end_date = dateControl2.valueAsNumber;
+  console.log(end_date);
+  //console.log(${start_date});
+
+
+
+
+  let brand_name;
+  var table = document.getElementById('myTable');
+  brand_json.result.forEach(async (element) => {
+    brand_name = element.brandname;
+    //console.log( brand_name);
+
+
+    const api_url = `/data/${start_date}/${end_date}/${brand_name}`;
+    const Data_response = await fetch(api_url);
+    const brand_data_json = await Data_response.json();
+
+    var row = `<tr>
+							<td>${brand_name}</td>
+							<td>${brand_data_json.result.profiles.length}</td>
+							<td>${brand_data_json.result.kpis.total_fans.current_period.value}</td>
+              <td>${brand_data_json.result.kpis.total_engagement.current_period.value}</td>
+					  </tr>`
+    table.innerHTML += row
+
+    //console.log("total engagement");
+    //console.log(brand_data_json.result.kpis.total_engagement.current_period.value
+  });
+
+  //console.log("total followers");
+  //console.log(brand_data_json.result.kpis.total_fans.current_period.value);
+
+  //console.log("total profiles");
+  //console.log(brand_data_json.result.profiles.length);
+  console.log("Successfully imported brand data.");
+  //plang, hai lori poti
+
+
+
+  //console.log(brand_data_json.result);
+});
+
+
+
+/*async function testing() {
+  console.log("Importing brands...");
+
+  const response = await fetch("/brands");
+  const json = await response.json();
+
+  console.log("Successfully imported brands.");
+
+  json.result.forEach(async (element) => {
+    const response2 = await fetch("/data");
+    const json2 = await response2.json();
+    console.log(json2);
+
+    console.log(json.result);
+  }); */
+//console.log(json.result);
+
+//let numberofbrands = response.result.lenght;
+
+//const response = await fetch("/data");
+//const json = await response.json();
+//console.log(json);
+
+//testing();
