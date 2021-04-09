92fdf90c3c58c5a0701dc04d723bd9f987952788
lorenavaduva <lorena.vaduva413@gmail.com>
2021-04-09 07:00:51 +0300

    Final commit

--- Brand_Compare_Backend.js
+++ Brand_Compare_Backend.js
@@ -1,18 +1,19 @@
-//I am using express(web framework for node.js) to host static files.
+
 const express = require("express");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-console.log(process.env);
+
 
 const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public"));
-//app.use(express.json({ limit: "1mb" }));
 
+
+//retrieve brands JSON from API 
 app.get("/brands", async (request, response) => {
   const fetchResponse = await fetch("https://app.socialinsider.io/api", {
-    // fetch getbrands data from API
+    
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -26,12 +27,14 @@ app.get("/brands", async (request, response) => {
     }),
   });
   const jsonrpc = await fetchResponse.json();
-  console.log(jsonrpc);
+  //console.log(jsonrpc);
   response.json(jsonrpc);
 });
 
+//retrieve brand data JSON from API
 app.get("/data/:start_date/:end_date/:brand_name",async (request, response) => {
-    const StartDate = Number(request.params.start_date);
+    
+  const StartDate = Number(request.params.start_date);
     const EndDate = Number(request.params.end_date);
     const BrandName = request.params.brand_name;
 
@@ -70,10 +73,3 @@ app.get("/data/:start_date/:end_date/:brand_name",async (request, response) => {
 
 
 
-
-/*app.post('/api', (request, response) => {
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
-    response.json(data);
-  });*/
--- public/sketch.js
+++ public/sketch.js
@@ -1,111 +1,57 @@
-let flag = 0;
+const table = document.getElementById("myTable");
+//storing brand names in a global variable
+//after importing brands from API
+const brands_array = [];
+
+async function createTable() {
+  console.log("Importing brands...");
+  const response = await fetch("/brands");
+  const brand_json = await response.json();
+  let number_of_brands = brand_json.result.length;
+  //console.log(brand_json);
+
+  for (i = 0; i < number_of_brands; i++) {
+    brands_array.push(brand_json.result[i].brandname);
+    var row = `<tr>
+    <td>${brand_json.result[i].brandname}</td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>`;
+    table.innerHTML += row;
+  }
+}
+
+//Creating initial table and fetching brands JSON from API
+createTable();
 
 document.getElementById("submit").addEventListener("click", async (event) => {
-  var table = document.getElementById("myTable");
+  //retrieving data input
+  const Start_input_data = document.getElementById("start");
+  const End_input_data = document.getElementById("end");
+  let start_date = Start_input_data.valueAsNumber;
+  let end_date = End_input_data.valueAsNumber;
 
-  //const start_date = dateControl.value.split('-');// year month day
-  //console.log(dateControl.valueAsNumber);
-  //console.log(dateControl.value);
-
-  const dateControl = document.getElementById("start");
-  const dateControl2 = document.getElementById("end");
-  let start_date = dateControl.valueAsNumber;
-  let end_date = dateControl2.valueAsNumber;
-
+  //check data validation
   if (start_date > end_date) {
     console.log("error");
     return false;
   }
 
-  //console.log(dateControl2.valueAsNumber);
-
-  console.log("Importing brands...");
-
-  const response = await fetch("/brands");
-  const brand_json = await response.json();
-  //console.log(brand_json);
-  console.log("Successfully imported brands.");
-
-  console.log("Importing brand data...");
-
-  let number_of_brands = brand_json.result.length;
-  console.log(number_of_brands);
-  //console.log(start_date);
-
-  //console.log(end_date);
-  //console.log(${start_date});
-
   let brand_name;
-
-  for (i = 0; i < number_of_brands; i++) {
-    brand_name = brand_json.result[i].brandname;
-    console.log(brand_name);
+  console.log("Importing brand data...");
+  for (i = 0; i < brands_array.length; i++) {
+    brand_name = brands_array[i];
 
     const api_url = `/data/${start_date}/${end_date}/${brand_name}`;
     const Data_response = await fetch(api_url);
     const brand_data_json = await Data_response.json();
     console.log(brand_data_json.result);
 
-    if (flag == 1) {
-      var x = document.getElementById("myTable").rows[i].cells;
+    var x = document.getElementById("myTable").rows[i].cells;
 
-      x[1].innerHTML = `${brand_data_json.result.profiles.length}`;
-      x[2].innerHTML = `${brand_data_json.result.kpis.total_fans.current_period.value}`;
-      x[3].innerHTML = `${brand_data_json.result.kpis.total_engagement.current_period.value}`;
-    } else {
-      var row = `<tr>
-							<td>${brand_data_json.result.brandname}</td>
-							<td>${brand_data_json.result.profiles.length}</td>
-							<td>${brand_data_json.result.kpis.total_fans.current_period.value}</td>
-              <td>${brand_data_json.result.kpis.total_engagement.current_period.value}</td>
-					  </tr>`;
-      table.innerHTML += row;
-    }
-
-    //console.log( element.brandname);
-    /*var row = `<tr>
-							<td>${brand_data_json.result.brandname}</td>
-							<td>${brand_data_json.result.profiles.length}</td>
-							<td>${brand_data_json.result.kpis.total_fans.current_period.value}</td>
-              <td>${brand_data_json.result.kpis.total_engagement.current_period.value}</td>
-					  </tr>`;
-    table.innerHTML += row; */
-
-    //console.log("total engagement");
-    //console.log(brand_data_json.result.kpis.total_engagement.current_period.value
+    x[1].innerHTML = `${brand_data_json.result.profiles.length}`;
+    x[2].innerHTML = `${brand_data_json.result.kpis.total_fans.current_period.value}`;
+    x[3].innerHTML = `${brand_data_json.result.kpis.total_engagement.current_period.value}`;
   }
-
-  //console.log("total followers");
-  //console.log(brand_data_json.result.kpis.total_fans.current_period.value);
-
-  //console.log("total profiles");
-  //console.log(brand_data_json.result.profiles.length);
-  //console.log("Successfully imported brand data.");
-  flag = 1;
-  //plang, hai lori poti
 });
-
-/*async function testing() {
-  console.log("Importing brands...");
-
-  const response = await fetch("/brands");
-  const json = await response.json();
-
-  console.log("Successfully imported brands.");
-
-  json.result.forEach(async (element) => {
-    const response2 = await fetch("/data");
-    const json2 = await response2.json();
-    console.log(json2);
-
-    console.log(json.result);
-  }); */
-//console.log(json.result);
-
-//let numberofbrands = response.result.lenght;
-
-//const response = await fetch("/data");
-//const json = await response.json();
-//console.log(json);
-
-//testing();
